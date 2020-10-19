ef3cfb770f3ce4ef60d783ba0311af5320e05e6d
Jaehyun Lee <buryeye7@naver.com>
2020-10-19 05:00:49 +0000

    r

--- gaiapy/cosmospy/__init__.py
+++
@@ -1,9 +0,0 @@
-from cosmospy._transaction import Transaction  # noqa: F401
-from cosmospy._wallet import (  # noqa: F401
-    generate_wallet,
-    privkey_to_address,
-    privkey_to_pubkey,
-    pubkey_to_address,
-)
-
-__version__ = "4.0.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
